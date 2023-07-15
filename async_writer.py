b188553448fdc34396ede096e36876c2b81405a9
yasu-a <yasuhiro.gtgt@gmail.com>
2023-07-15 22:00:15 +0900

    update performance fix

--- async_writer.py
+++ async_writer.py
@@ -2,6 +2,8 @@ import multiprocessing as mp
 import time
 import imageio as iio
 
+MAX_BACKLOG_FRAMES = 128
+
 
 def _worker(q: mp.Queue, params: dict):
     out = iio.get_writer(
@@ -10,11 +12,12 @@ def _worker(q: mp.Queue, params: dict):
         fps=params['fps'],
         mode='I',
         codec='h264',
+        quality=4
     )
 
     while True:
         if q.empty():
-            time.sleep(0.1)
+            time.sleep(0.01)
         else:
             frame = q.get(block=False)
             if frame is None:
@@ -26,11 +29,13 @@ def _worker(q: mp.Queue, params: dict):
 
 class AsyncVideoFrameWriter:
     def __init__(self, path, fps):
-        self.__q = mp.Queue()
+        self.__q = mp.Queue(maxsize=MAX_BACKLOG_FRAMES)
         params = dict(path=path, fps=fps)
         self.__p = mp.Process(target=_worker, args=(self.__q, params))
 
     def write(self, frame):
+        if not self.__p.is_alive():
+            raise ValueError('writer not open')
         self.__q.put(frame)
 
     def __enter__(self):
