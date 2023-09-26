c9cd6c9a8888244b4791cd98f1c1b201406bac08
yasu-a <yasuhiro.gtgt@gmail.com>
2023-09-26 22:27:01 +0900

    remove prepared dataset

--- util.py
+++ util.py
@@ -1,44 +1,7 @@
-import json
-import os
-
 import numpy as np
 from skimage.feature import peak_local_max
 
-from main_diff_generator_bug_fix import MEMMAP_PATH
-
 np.set_printoptions(suppress=True)
-
-
-def load():
-    motions = np.memmap(
-        os.path.join(MEMMAP_PATH, 'motions.map'),
-        mode='r',
-        dtype=np.uint8
-    )
-    originals = np.memmap(
-        os.path.join(MEMMAP_PATH, 'originals.map'),
-        mode='r',
-        dtype=np.uint8
-    )
-    tss = np.memmap(
-        os.path.join(MEMMAP_PATH, 'tss.map'),
-        mode='r',
-        dtype=np.float32
-    )
-
-    with open(os.path.join(MEMMAP_PATH, 'shape.json'), 'r') as f:
-        shape_json = json.load(f)
-
-    motions = motions.reshape(shape_json['motions'])
-    originals = originals.reshape(shape_json['originals'])
-    tss = tss.reshape(shape_json['tss'])
-
-    print(f'{motions.shape=} {originals.shape=} {tss.shape=}')
-
-    return motions, originals, tss
-
-
-motions, originals, tss = load()
 
 
 def slice_frame(fr, rect):
