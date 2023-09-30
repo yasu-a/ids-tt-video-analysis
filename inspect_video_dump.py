5a46753eb7afec1e44ae86b0343655c9bd7c04a2
yasu-a <yasuhiro.gtgt@gmail.com>
2023-09-30 23:26:32 +0900

    cleaned code up

--- inspect_video_dump.py
+++ inspect_video_dump.py
@@ -2,12 +2,12 @@ import sys
 
 import numpy as np
 
-np.set_printoptions(suppress=True)
-
 import dataset
 
+np.set_printoptions(suppress=True)
 
-def inspect(video_name, rect, start=None, stop=None):
+
+def inspect(video_name, start=None, stop=None):
     assert start is None, 'providing start is forbidden'
 
     with dataset.VideoFrameStorage(
@@ -28,17 +28,13 @@ def inspect(video_name, rect, start=None, stop=None):
 
 
 def main(video_name):
-    rect = slice(70, 260), slice(180, 255)  # height, width
-    # height: 奥の選手の頭から手前の選手の足がすっぽり入るように
-    # width: ネットの部分の卓球台の幅に合うように
-
-    inspect(video_name, rect)
+    inspect(video_name)
 
 
 if __name__ == '__main__':
     _, *args = sys.argv
     if args:
-        video_name = args[0]
+        vn = args[0]
     else:
-        video_name = None
-    main(video_name=video_name)
+        vn = None
+    main(video_name=vn)
