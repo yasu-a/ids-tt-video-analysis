4f538e300aae375295822d3009e2fc09e32d6224
yasu-a <yasuhiro.gtgt@gmail.com>
2023-10-03 09:01:00 +0900

    recover config with platform name

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+import platform
+
+device_name = platform.node()
+
+if device_name == 'unknown':
+    VIDEO_DIR_PATH = os.path.expanduser(r'~/Desktop/idsttvideos/singles')
+    FRAME_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\frames'
+    MOTION_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\motions'
+    DEFAULT_VIDEO_NAME = '20230205_04_Narumoto_Harimoto'
+    FEATURE_CACHE_PATH = r'D:\idstt\iDSTTVideoFrameDump\cache'
+elif device_name == 'DESKTOP-O6M276J':
+    VIDEO_DIR_PATH = os.path.expanduser(r'~/Desktop/idsttvideos/singles')
+    FRAME_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\frames'
+    MOTION_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\motions'
+    DEFAULT_VIDEO_NAME = '20230205_04_Narumoto_Harimoto'
+    FEATURE_CACHE_PATH = r'D:\idstt\iDSTTVideoFrameDump\cache'
+else:
+    assert False, device_name
