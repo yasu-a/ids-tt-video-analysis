ca81f72f761fd93a891c2a9d9bceb77199a2b7d0
yasu-a <yasuhiro.gtgt@gmail.com>
2023-10-03 09:02:38 +0900

    update 'DESKTOP-O6M276J'

--- config.py
+++ config.py
@@ -9,11 +9,11 @@ if device_name == 'unknown':
     MOTION_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\motions'
     DEFAULT_VIDEO_NAME = '20230205_04_Narumoto_Harimoto'
     FEATURE_CACHE_PATH = r'D:\idstt\iDSTTVideoFrameDump\cache'
-elif device_name == 'DESKTOP-O6M276J':
+elif device_name == 'DESKTOP-O6M276J':  # Desktop of yasu.a
     VIDEO_DIR_PATH = os.path.expanduser(r'~/Desktop/idsttvideos/singles')
-    FRAME_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\frames'
-    MOTION_DUMP_DIR_PATH = r'D:\idstt\iDSTTVideoFrameDump\motions'
+    FRAME_DUMP_DIR_PATH = r'H:\idstt\iDSTTVideoFrameDump\frames'
+    MOTION_DUMP_DIR_PATH = r'H:\idstt\iDSTTVideoFrameDump\motions'
     DEFAULT_VIDEO_NAME = '20230205_04_Narumoto_Harimoto'
-    FEATURE_CACHE_PATH = r'D:\idstt\iDSTTVideoFrameDump\cache'
+    FEATURE_CACHE_PATH = r'H:\idstt\iDSTTVideoFrameDump\cache'
 else:
     assert False, device_name
