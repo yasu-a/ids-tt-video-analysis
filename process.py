d11694e372d8e8895db95c0e2e2f0948ddddb54f
yasu-a <yasuhiro.gtgt@gmail.com>
2023-11-14 20:34:23 +0900

    reordered methods

--- process.py
+++ process.py
@@ -4,11 +4,11 @@ import argparse
 class ProcessStage:
     NAME = None
 
-    def run(self):
-        raise NotImplementedError()
-
     @classmethod
     def customize_parser(cls, parser: argparse.ArgumentParser) -> None:
+        raise NotImplementedError()
+
+    def run(self):
         raise NotImplementedError()
 
 
