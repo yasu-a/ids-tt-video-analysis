5d011131e3f4752f02f2d8699f537bbf4c078065
yasu-a <yasuhiro.gtgt@gmail.com>
2023-11-27 05:09:04 +0900

    added primitive motion visualizer

--- main_debug.py
+++ main_debug.py
@@ -1,21 +1,25 @@
-import main
+if __name__ == '__main__':
+    import main
 
-run_name = 'primitive-motion-dump-debug'
+    run_name = 'primitive-motion-visualization-debug'
 
-if run_name == 'marker-full-restore':
-    import shutil
+    if run_name == 'marker-full-restore':
+        import shutil
 
-    try:
-        shutil.rmtree('./label_data/markers')
-    except FileNotFoundError:
-        pass
+        try:
+            shutil.rmtree('./label_data/markers')
+        except FileNotFoundError:
+            pass
 
-    argv = ['mi', './label_data/__marker_source_json_v2/*.json']
+        argv = ['mi', './label_data/__marker_source_json_v2/*.json']
 
-elif run_name == 'primitive-motion-dump-debug':
-    argv = ['pmd', '20230219_03_Narumoto_Ito']
+    elif run_name == 'primitive-motion-dump-debug':
+        argv = ['pmd', '20230219_03_Narumoto_Ito']
 
-else:
-    assert False, run_name
+    elif run_name == 'primitive-motion-visualization-debug':
+        argv = ['vpm', '20230219_03_Narumoto_Ito', '--start', '300', '--stop', '600']
 
-main.run(argv)
+    else:
+        assert False, run_name
+
+    main.run(argv)
---
+++ process_visualize_primitive_motion.py
@@ -0,0 +1,98 @@
+import argparse
+
+import cv2
+import numpy as np
+from tqdm import tqdm
+
+import app_logging
+import async_writer
+import npstorage_context as snp_context
+import process
+import storage
+import storage.npstorage as snp
+
+logger = app_logging.create_logger(__name__)
+
+
+class ProcessStagePrimitiveMotionDump(process.ProcessStage):
+    NAME = 'visualize-primitive-motion'
+    ALIASES = 'vpm',
+
+    @classmethod
+    def customize_parser(cls, parser: argparse.ArgumentParser) -> None:
+        parser.add_argument('video_name', type=str)
+        parser.add_argument('--start', type=int, default=None)
+        parser.add_argument('--stop', type=int, default=None)
+        parser.add_argument('--out-path', type=str, default='./vpm-out.mp4')
+
+    def __init__(
+            self,
+            video_name: str,
+            start: int,
+            stop: int,
+            out_path: str
+    ):
+        self.__video_name = video_name
+        logger.info(f'{video_name=!r}')
+
+        with storage.create_instance(
+                domain='numpy_storage',
+                entity=self.__video_name,
+                context='primitive_motion',
+                mode='r',
+        ) as snp_primitive_motion:
+            assert isinstance(snp_primitive_motion, snp.NumpyStorage)
+            count = snp_primitive_motion.count()
+
+        start = start or 0
+        stop = stop or count - 1
+        self.__start, self.__stop = start, stop
+        logger.info(f'{start=}, {stop=}')
+
+        self.__out_path = out_path
+        logger.info(f'{out_path=!r}')
+
+    def run(self):
+        with storage.create_instance(
+                domain='numpy_storage',
+                entity=self.__video_name,
+                context='frames',
+                mode='r',
+        ) as snp_video_frames:
+            assert isinstance(snp_video_frames, snp.NumpyStorage)
+
+            with storage.create_instance(
+                    domain='numpy_storage',
+                    entity=self.__video_name,
+                    context='primitive_motion',
+                    mode='r',
+            ) as snp_primitive_motion:
+                assert isinstance(snp_primitive_motion, snp.NumpyStorage)
+
+                timestamps = snp_primitive_motion.get_array('timestamp')
+                fps = 1 / np.nanmean(np.diff(timestamps))
+                logger.info(f'{fps=}')
+                assert fps > 0, fps
+
+                with async_writer.AsyncVideoFrameWriter(
+                        self.__out_path,
+                        fps=fps
+                ) as vw:
+                    for i in tqdm(range(self.__start, self.__stop)):
+                        pmd_entry = snp_primitive_motion.get_entry(i)
+                        vfd_entry = snp_video_frames.get_entry(i)
+                        assert isinstance(vfd_entry, snp_context.SNPEntryVideoFrame)
+                        assert isinstance(pmd_entry, snp_context.SNPEntryPrimitiveMotion)
+
+                        assert pmd_entry.fi == vfd_entry.fi, (pmd_entry.fi, vfd_entry.fi)
+
+                        image = vfd_entry.original.copy()
+                        image = cv2.resize(image, None, fx=2, fy=2)
+                        for m_start, m_end in zip(pmd_entry.start, pmd_entry.end):
+                            if np.any(np.isnan(m_start)):
+                                break
+                            y1, x1 = m_start.astype(int) * 2
+                            y2, x2 = m_end.astype(int) * 2
+                            cv2.arrowedLine(image, (x1, y1), (x2, y2), (0, 255, 255), 2)
+
+                        vw.write(image)
