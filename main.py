4e9a0e6b39b45fd92f608b7fe0f690c83d85bb5d
yasu-a <yasuhiro.gtgt@gmail.com>
2023-11-27 05:55:35 +0900

    bug-fix

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import process_extract_rect_cli
 import process_marker_import
 import process_primitive_motion_dump
+import process_primitive_motion_visualize
 import process_video_dump
 from process import register_process_in_module, run
 
@@ -8,7 +9,7 @@ register_process_in_module(process_video_dump)
 register_process_in_module(process_extract_rect_cli)
 register_process_in_module(process_marker_import)
 register_process_in_module(process_primitive_motion_dump)
-register_process_in_module(process_visualize_primitive_motion)
+register_process_in_module(process_primitive_motion_visualize)
 
 if __name__ == '__main__':
     run()
