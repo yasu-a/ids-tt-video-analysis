2e0aec4c1f884e074417a481c537bb6d0208bde2
yasu-a <yasuhiro.gtgt@gmail.com>
2023-12-13 02:46:06 +0900

    renamed file name

--- util_ground_truth_generator.py
+++ util_ground_truth_generator.py
@@ -1,7 +1,6 @@
 import functools
 import os.path
 from pprint import pformat
-from pprint import pprint
 
 import cv2
 import numpy as np
@@ -10,7 +9,6 @@ import pandas as pd
 import app_logging
 import npstorage_context as snp_context
 import storage
-import storage.npstorage as snp
 from config import config
 from label_manager.frame_label.factory import VideoFrameLabelFactory
 from label_manager.frame_label.sample_set import FrameAggregationResult
@@ -125,100 +123,17 @@ class GrandTruthGenerator:
         df.to_csv(path)
 
 
-def main():
-    video_name_lst = [
-        '20230205_04_Narumoto_Harimoto',
-        '20230219_03_Narumoto_Ito',
-        '20230225_02_Matsushima_Ando'
-    ]
-
-    for video_name in video_name_lst:
-        gtg = GrandTruthGenerator(VideoFrameLabelFactory.create_instance())
-        gtg.dump_grand_truth_dataframe(video_name)
-    return
-
-    ds = VideoFrameLabelFactory.create_instance()
-    pprint(ds)
-    s = ds[video_name]
-    pprint(ds[video_name][0].label_json)
-
-    agg: FrameAggregationResult = s.aggregate_full()
-
-    ordered_label = ['Stay', 'Play', 'Ready']
-
-    result_lst = list(
-        agg.extract_ordered_label_groups(
-            label_order=[
-                s.frame_label_name_list.index(ln)
-                for ln in ordered_label
-            ],
-            predicate=lambda entry: entry.reliability > 0.5
-        )
-    )
-
-    with storage.create_instance(
-            domain='numpy_storage',
-            entity=video_name,
-            context='frames',
-            mode='r',
-    ) as snp_video_frame:
-        assert isinstance(snp_video_frame, snp.NumpyStorage)
-        src_fis = snp_video_frame.get_array('fi', fill_nan=-1)
-        src_timestamps = snp_video_frame.get_array('timestamp')
-
-        src_fi_ts_lut = {
-            fi: ts
-            for fi, ts in zip(src_fis, src_timestamps)
-        }
-
-        def find_nearest_timestamp(fi_target):
-            abs_fi_delta = 0
-            while True:
-                for fi in [fi_target + abs_fi_delta, fi_target - abs_fi_delta]:
-                    ts = src_fi_ts_lut.get(fi)
-                    if ts is not None:
-                        return ts
-                abs_fi_delta += 1
-
-    rows = []
-    for i, result in enumerate(result_lst):
-        fi_center_start = -1 if result.prev_frame is None else np.mean([
-            result.prev_frame.fi_center,
-            result.frames[0].fi_center
-        ])
-        fi_center_end = -1 if result.next_frame is None else np.mean([
-            result.next_frame.fi_center,
-            result.frames[-1].fi_center
-        ])
-        fi_centers = [f.fi_center for f in result.frames]
-        labels = [f.label_index for f in result.frames]
-        reliability = [f.reliability for f in result.frames]
-
-        rows.append(
-            dict(
-                fi_center_start=fi_center_start,
-                **{
-                    label_name: fi
-                    for label_name, fi in zip(
-                        [ordered_label[i] for i in labels],
-                        fi_centers
-                    )
-                },
-                fi_center_end=fi_center_end,
-                **{
-                    'ts_' + label_name: find_nearest_timestamp(fi)
-                    for label_name, fi in zip(
-                        [ordered_label[i] for i in labels],
-                        fi_centers
-                    )
-                },
-                reliability=reliability
-            )
-        )
-
-    df = pd.DataFrame(rows)
-    print(df)
-
-
 if __name__ == '__main__':
+    def main():
+        video_name_lst = [
+            '20230205_04_Narumoto_Harimoto',
+            '20230219_03_Narumoto_Ito',
+            '20230225_02_Matsushima_Ando'
+        ]
+
+        for video_name in video_name_lst:
+            gtg = GrandTruthGenerator(VideoFrameLabelFactory.create_instance())
+            gtg.dump_grand_truth_dataframe(video_name)
+
+
     main()
